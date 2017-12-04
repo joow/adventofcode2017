1afe64599bc83993e85f698d4f602acaa56ae784
Benoît Giraudou <bgiraudou@gmail.com>
2017-12-04 09:52:17 +0100

    ✨ Puzzle 2 Day 4

--- day4/src/main/kotlin/puzzle.kt
+++ day4/src/main/kotlin/puzzle.kt
@@ -2,9 +2,25 @@ class Passphrase(val words: List<String>) {
     fun isValid() = words.groupBy { it }.count() == words.count()
 
     companion object {
-        fun parse(s: String, transform: (s: String) -> List<String> = ::identity) =
+        fun parse(s: String, transform: (s: String) -> Iterable<String> = ::identity) =
             Passphrase(s.split(" ").flatMap { transform(it) })
     }
 }
 
 fun identity(s: String) = listOf(s)
+
+fun anagrams(s: String) = permutations(s.toList()).map { String(it.toCharArray()) }.groupBy { it }.keys
+
+fun <T> permutations(input: List<T>): List<List<T>> {
+    if (input.size == 1) return listOf(input)
+    val permutations = mutableListOf<List<T>>()
+    val toInsert = input[0]
+    for (perm in permutations(input.drop(1))) {
+        for (i in 0..perm.size) {
+            val newPermutation = perm.toMutableList()
+            newPermutation.add(i, toInsert)
+            permutations.add(newPermutation)
+        }
+    }
+    return permutations
+}
--- day4/src/test/kotlin/Test.kt
+++ day4/src/test/kotlin/Test.kt
@@ -1,4 +1,5 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Ignore
 import org.junit.Test
 
 class Tests {
@@ -29,14 +30,48 @@ class Tests {
         assertThat(passphrase.isValid()).isFalse()
     }
 
+    private fun readLines(path: String) =
+        Tests::class.java.getResource(path).readText().split("\n").filterNot { it.isBlank() }
+
     @Test
     fun `it should count valid passphrases for puzzle 1`() {
-        val input = Tests::class.java.getResource("input").readText()
-        val lines = input.split("\n").filterNot { it.isBlank() }
+        val lines = readLines("input")
 
         val passphrases = lines.map { Passphrase.parse(it) }
         val validPassphrases = passphrases.count { it.isValid() }
 
         assertThat(validPassphrases).isEqualTo(337)
+    }
+
+    @Test
+    fun `it should get all anagrams of a word`() {
+        val word = "abc"
+        val anagrams = anagrams(word)
+
+        assertThat(anagrams).hasSize(6)
+        assertThat(anagrams).contains("abc", "acb", "bac", "bca", "cab", "cba")
+    }
+
+    @Test
+    fun `it should detect valid anagrams passphrase`() {
+        assertThat(Passphrase.parse("abcde fghij", ::anagrams).isValid()).isTrue()
+        assertThat(Passphrase.parse("a ab abc abd abf abj", ::anagrams).isValid()).isTrue()
+        assertThat(Passphrase.parse("iiii oiii ooii oooi oooo", ::anagrams).isValid()).isTrue()
+    }
+
+    @Test
+    fun `it should detect invalid anagrams passphrase`() {
+        assertThat(Passphrase.parse("abcde xyz ecdab", ::anagrams).isValid()).isFalse()
+        assertThat(Passphrase.parse("oiii ioii iioi iiio", ::anagrams).isValid()).isFalse()
+    }
+
+    @Test
+    fun `it should count valid passphrases for puzzle 2`() {
+        val lines = readLines("input")
+
+        val passphrases = lines.map { Passphrase.parse(it, ::anagrams) }
+        val validPassphrases = passphrases.count { it.isValid() }
+
+        assertThat(validPassphrases).isEqualTo(231)
     }
 }
