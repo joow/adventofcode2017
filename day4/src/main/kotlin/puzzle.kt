85eb3b61cde94b6851197db1141e6dda16fdf10a
Benoît Giraudou <bgiraudou@gmail.com>
2017-12-04 09:20:29 +0100

    ♻️ Introduce transformation function to turn a string into a list of strings

--- day4/src/main/kotlin/puzzle.kt
+++ day4/src/main/kotlin/puzzle.kt
@@ -2,6 +2,9 @@ class Passphrase(val words: List<String>) {
     fun isValid() = words.groupBy { it }.count() == words.count()
 
     companion object {
-        fun parse(s: String) = Passphrase(s.split(" "))
+        fun parse(s: String, transform: (s: String) -> List<String> = ::identity) =
+            Passphrase(s.split(" ").flatMap { transform(it) })
     }
 }
+
+fun identity(s: String) = listOf(s)
