af65f2730838571f4e20b0ae68349ff022925a1e
Benoît Giraudou <bgiraudou@gmail.com>
2017-12-05 11:25:58 +0100

    ✨ Puzzle 2 Day 5

--- day5/src/main/kotlin/puzzle.kt
+++ day5/src/main/kotlin/puzzle.kt
@@ -1,10 +1,14 @@
-class Instructions(val jumps: List<Int>, val curr: Int = 0, val steps: Int = 0) {
-    fun jump(): Instructions {
-        val newCurr = curr + jumps[curr]
-        return Instructions(transform(), newCurr, steps + 1)
+class Instructions(val jumps: MutableList<Int>, val curr: Int = 0, val steps: Int = 0) {
+    fun jump(inc: (offset: Int) -> Int): Instructions {
+        val jump = jumps[curr]
+        return Instructions(transform(inc), curr + jump, steps + 1)
     }
 
-    private fun transform() = jumps.take(curr).plus(jumps[curr] + 1).plus(jumps.drop(curr + 1))
+    private fun transform(inc: (offset: Int) -> Int): MutableList<Int> {
+        jumps[curr] = inc(jumps[curr])
+        return jumps
+    }
+
 
     fun isOut() = curr >= jumps.size
 
@@ -12,5 +16,5 @@ class Instructions(val jumps: List<Int>, val curr: Int = 0, val steps: Int = 0) 
             jumps.mapIndexed { index, n -> if (index == curr) "($n)" else "$n" }.joinToString()
 }
 
-fun steps(instructions: Instructions) =
-        generateSequence(instructions) { it.jump() }.first { it.isOut() }.steps
+fun steps(instructions: Instructions, inc: (offset: Int) -> Int = { it + 1 }) =
+        generateSequence(instructions) { it.jump(inc) }.first { it.isOut() }.steps
--- day5/src/test/kotlin/Test.kt
+++ day5/src/test/kotlin/Test.kt
@@ -4,9 +4,9 @@ import org.junit.Test
 class Tests {
     @Test
     fun `it should jump to next instruction`() {
-        val instructions = Instructions(listOf(0, 3))
+        val instructions = Instructions(mutableListOf(0, 3))
 
-        val newInstructions = instructions.jump()
+        val newInstructions = instructions.jump({ it + 1 })
 
         assertThat(newInstructions.jumps).containsExactly(1, 3)
         assertThat(newInstructions.curr).isEqualTo(0)
@@ -15,16 +15,16 @@ class Tests {
 
     @Test
     fun `it should jump to next negative instruction`() {
-        val instructions = Instructions(listOf(0, 3, 0, 1, -3), 4, 0)
+        val instructions = Instructions(mutableListOf(0, 3, 0, 1, -3), 4, 0)
 
-        val newInstructions = instructions.jump()
+        val newInstructions = instructions.jump({ it + 1 })
 
         assertThat(newInstructions.jumps).containsExactly(0, 3, 0, 1, -2)
     }
 
     @Test
     fun `it should count steps to exit instructions`() {
-        val instructions = Instructions(listOf(0, 3, 0, 1, -3))
+        val instructions = Instructions(mutableListOf(0, 3, 0, 1, -3))
 
         val steps = steps(instructions)
 
@@ -34,10 +34,29 @@ class Tests {
     @Test
     fun `it should count steps for puzzle 1`() {
         val input = Tests::class.java.getResource("input").readText()
-        val instructions = Instructions(input.split("\n").filterNot { it.isBlank() }.map { it.toInt() })
+        val instructions = Instructions(input.split("\n").filterNot { it.isBlank() }.map { it.toInt() }.toMutableList())
 
         val steps = steps(instructions)
 
         assertThat(steps).isEqualTo(381680)
+    }
+
+    @Test
+    fun `it should count steps to exit instructions with increment or decrement rule`() {
+        val instructions = Instructions(mutableListOf(0, 3, 0, 1, -3))
+
+        val steps = steps(instructions) { if (it >= 3) it - 1 else it + 1 }
+
+        assertThat(steps).isEqualTo(10)
+    }
+
+    @Test
+    fun `it should count steps for puzzle 2`() {
+        val input = Tests::class.java.getResource("input").readText()
+        val instructions = Instructions(input.split("\n").filterNot { it.isBlank() }.map { it.toInt() }.toMutableList())
+
+        val steps = steps(instructions) { if (it >= 3) it - 1 else it + 1 }
+
+        assertThat(steps).isEqualTo(29717847)
     }
 }
