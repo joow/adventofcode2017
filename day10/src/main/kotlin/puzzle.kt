750b1a00d3b76b683f4d03e5ec287cb14f4332b5
Benoît Giraudou <bgiraudou@gmail.com>
2017-12-10 14:56:32 +0100

    ✨ Puzzle 2 Day 10

--- day10/src/main/kotlin/puzzle.kt
+++ day10/src/main/kotlin/puzzle.kt
@@ -17,8 +17,29 @@ class KnotHash(size: Int = 256,
     }
 }
 
-fun knotHash(lengths: List<Int>, size: Int = 256): KnotHash {
+fun knotHash(lengths: List<Int>, size: Int = 256, rounds: Int = 1): KnotHash {
     var hash = KnotHash(size)
-    for (length in lengths) hash = hash.hash(length)
+    repeat(rounds) { for (length in lengths) hash = hash.hash(length) }
     return hash.shift()
+}
+
+fun denseHash(lengths: List<Int>, size: Int = 256): List<Int> {
+    val hash = knotHash(lengths, size, 64)
+
+    return hash.state.windowed(16, 16).map { xor(it) }
+}
+
+fun xor(xs: List<Int>): Int = xs.fold(0) { acc, n -> acc xor n}
+
+fun ascii(s: String) = s.map { it.toInt() }
+
+val lengthsSuffix = listOf(17, 31, 73, 47, 23)
+
+fun lengths(s: String) = ascii(s).plus(lengthsSuffix)
+
+fun hex(xs: List<Int>) = xs.joinToString("") { it.toHexString() }
+
+private fun Int.toHexString(): String {
+    val hex = toString(16)
+    return if (hex.length == 2) hex else "0$hex"
 }
--- day10/src/test/kotlin/Test.kt
+++ day10/src/test/kotlin/Test.kt
@@ -1,6 +1,8 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
+const val PUZZLE_INPUT = "106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118"
+
 class Tests {
 
     @Test
@@ -48,11 +50,47 @@ class Tests {
 
     @Test
     fun `it should solve puzzle 1`() {
-        val input = "106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118"
-        val lengths = input.split(",").map { it.toInt() }
+        val lengths = PUZZLE_INPUT.split(",").map { it.toInt() }
 
         val hash = knotHash(lengths)
 
-        assertThat(hash.state[0] * hash.state[1]).isEqualTo(100)
+        assertThat(hash.state[0] * hash.state[1]).isEqualTo(6909)
+    }
+
+    @Test
+    fun `it should convert input to ASCII codes`() {
+        val lengths = ascii("1,2,3")
+
+        assertThat(lengths).containsExactly(49, 44, 50, 44, 51)
+    }
+
+    @Test
+    fun `it should compute lengths from input`() {
+        val lengths = lengths("1,2,3")
+
+        assertThat(lengths).containsExactly(49, 44, 50, 44, 51, 17, 31, 73, 47, 23)
+    }
+
+    @Test
+    fun `it should xor values`() {
+        val xor = xor(listOf(65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22))
+
+        assertThat(xor).isEqualTo(64)
+    }
+
+    @Test
+    fun `it should display hex value of list of ints`() {
+        val hex = hex(listOf(64, 7, 255))
+
+        assertThat(hex).isEqualTo("4007ff")
+    }
+
+    @Test
+    fun `it should solve puzzle 2`() {
+        val lengths = lengths(PUZZLE_INPUT)
+        val denseHash = denseHash(lengths)
+        val hex = hex(denseHash)
+
+        assertThat(hex).isEqualTo("9d5f4561367d379cfbf04f8c471c0095")
     }
 }
